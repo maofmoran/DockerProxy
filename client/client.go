ceae782e694786a5f45abe8376304d2be3051c26
aviravitz <72856129+aviravitz@users.noreply.github.com>
2021-01-14 12:27:08 +0200

    client build in docker

--- client/client.go
+++ client/client.go
@@ -3,13 +3,27 @@ package main
 import (
 	"log"
 	"net/http"
+	"os/exec"
 	"time"
 )
 
 func main() {
+
+	if err := exec.Command("route", "add", "default", "gw", "192.168.2.150", "eth0").Run(); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Print("added 150 as dg")
+	}
+
+	if err := exec.Command("route", "del", "default", "gw", "192.168.2.1").Run(); err != nil {
+		log.Fatal(err)
+	} else {
+		log.Print("removed 1 as dg")
+	}
+
 	for {
 		time.Sleep(5 * time.Second)
-		resp, err := http.Get("http://192.168.2.150:8080/ping")
+		resp, err := http.Get("http://192.168.2.150:8080/192.168.1.10:8080/ping")
 		if err != nil {
 			log.Printf("Error: %v", err)
 		} else {
